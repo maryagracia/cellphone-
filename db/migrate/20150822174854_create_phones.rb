846211ed412c6ac35dcf5d910deeb62f4b3a66fb
elena <elenigavi06@gmail.com>
2015-08-22 14:59:35 -0500

    proyecto git inicial

---
+++ db/migrate/20150822174854_create_phones.rb
@@ -0,0 +1,11 @@
+class CreatePhones < ActiveRecord::Migration
+  def change
+    create_table :phones do |t|
+      t.string :name
+      t.text :description
+      t.string :picture
+
+      t.timestamps
+    end
+  end
+end
